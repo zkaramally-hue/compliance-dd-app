d7ab971a57ace371c7fe0c916d75ffe5bd5d48d4
zkaramally-hue <zkaramally@gmail.com>
2026-01-08 13:15:23 -0600

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+javascriptexport const metadata = {
+  title: 'Compliance Due Diligence Platform',
+  description: 'Industry-Tailored Background Intelligence',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
